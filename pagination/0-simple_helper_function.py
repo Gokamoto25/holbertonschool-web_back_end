31810211e3792463445913e71096ad7962488876
Gokamoto25 <okamoto.direc@gmail.com>
2024-02-05 10:04:02 -0800

    modified:   0-simple_helper_function.py

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,6 +5,13 @@ def index_range(page: int, page_size: int) -> tuple:
     """
     Devuelve una tupla de los índices inicial
     y final para una página y un tamaño de página determinados.
+    
+    Parametros:
+    - pagina (int): el numero de pagina (basado en 1).
+    - page_size (int): el tamaño de cada página.
+
+    Return:
+    tupla: una tupla que contiene los índices inicial y final.
     """
     start_index = (page - 1) * page_size
     end_index = page * page_size
@@ -15,6 +22,8 @@ if __name__ == "__main__":
     """
     se ejecutará solo si el script se ejecuta como programa
     principal
+
+    Atributos: - res: Variable utilizada para almacenar el resultado de llamar a la función index_range.
     """
     res = index_range(1, 7)
     print(type(res))  # <class 'tuple'>
