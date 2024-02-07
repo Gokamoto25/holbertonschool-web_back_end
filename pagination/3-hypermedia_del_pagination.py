d4bf7f3d332f0a2213c8c67d77e57d826c6ff1bf
Gokamoto25 <okamoto.direc@gmail.com>
2024-02-07 01:34:24 -0800

    modified:   3-hypermedia_del_pagination.py

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -40,7 +40,9 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-    """Retorna un diccionario con la informacion hymermedia del
+    """
+    
+    Retorna un diccionario con la informacion hymermedia del
     pagination para el index y pagesize especifico
     """
     len_indexed_dataset = len(self.__indexed_dataset)
