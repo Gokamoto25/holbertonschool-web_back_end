8b4be31a8a037fd3c8ceda42b086c65e91d4b437
Gokamoto25 <okamoto.direc@gmail.com>
2024-02-07 01:12:34 -0800

    modified:   0-simple_helper_function.py

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,49 +1,16 @@
 #!/usr/bin/env python3
 
+"""modulo para calcular rango del indice"""
 
-def index_range(page: int, page_size: int) -> tuple:
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    Devuelve una tupla de los índices inicial
-    y final para una página y un tamaño de página determinados.
-
-    Parametros:
-    - pagina (int): el numero de pagina (basado en 1).
-    - page_size (int): el tamaño de cada página.
-
-    Return:
-    tupla: una tupla que contiene los índices inicial y final.
-
-    Ejemplo:
-    >>> index_range(1, 7)
-    (0, 7)
-
-    >>> index_range(page=3, page_size=15)
-    (30, 45)
+    Retorna una tupla (start_index, end_index)
+    para los valores dados de page y pagesize
     """
     start_index = (page - 1) * page_size
-    end_index = page * page_size
+    end_index = start_index + page_size
+
     return start_index, end_index
-
-
-if __name__ == "__main__":
-    """
-    se ejecutará solo si el script se ejecuta como programa
-    principal
-
-    Example:
-    >>> python script.py
-    El código dentro de este bloque se ejecutará.
-
-    Atributos: - res: Variable utilizada para almacenar el
-    resultado de llamar a la función index_range.
-
-    Uso: Este bloque se usa comúnmente para probar o ejecutar
-    código específico cuando el script se ejecuta directamente.
-    """
-    res = index_range(1, 7)
-    print(type(res))  # <class 'tuple'>
-    print(res)        # (0, 7)
-
-    res = index_range(page=3, page_size=15)
-    print(type(res))  # <class 'tuple'>
-    print(res)        # (30, 45)
